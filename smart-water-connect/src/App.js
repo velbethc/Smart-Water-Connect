fde54412278b0b4ff3296d2c1dabf0e524a388d4
Velbeth Cifuentes <velbethcifuentes@velbeths-mbp.myfiosgateway.com>
2022-10-29 21:21:44 -0400

    test

--- smart-water-connect/src/App.js
+++ smart-water-connect/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey Harris! If you are seeing this page, you have successfully deployed your first React app to AWS Amplify!
         </p>
         <a
           className="App-link"
