ebad1672e90559fadcbc7dd830e3fbe5523891dc
shirali-patel <113568326+shirali-patel@users.noreply.github.com>
2022-10-31 17:12:13 -0400

    added names (#2)
    Co-authored-by: Shirali Patel <shiralipatel@Shiralis-MacBook-Pro.local>

--- smart-water-connect/src/App.js
+++ smart-water-connect/src/App.js
@@ -6,6 +6,9 @@ function App() {
     <h1>
     This is the webapp for the Smart Water Connect project
     </h1>
+    <h2>
+    Shirali, Velbeth, and Haris will be working on this project.
+    </h2>
     </div>
   );
 }
