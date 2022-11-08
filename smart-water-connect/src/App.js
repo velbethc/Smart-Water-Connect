116e40654c9d3e7b1d1bb2144ccbf94ad6aa46b9
velbethc <63129903+velbethc@users.noreply.github.com>
2022-11-08 16:38:59 -0500

    added a navbar (#3)
    Co-authored-by: Velbeth Cifuentes <velbethcifuentes@Velbeths-MacBook-Pro.local>

--- smart-water-connect/src/App.js
+++ smart-water-connect/src/App.js
@@ -1,14 +1,18 @@
-import './App.css';
-
+import "./App.css";
+import Navbar from "./Navbar";
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
     <h1>
     This is the webapp for the Smart Water Connect project
     </h1>
     <h2>
     Shirali, Velbeth, and Haris will be working on this project.
     </h2>
+=======
+      <Navbar/>
+>>>>>>> Stashed changes
     </div>
   );
 }
